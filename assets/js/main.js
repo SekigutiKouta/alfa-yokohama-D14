708d3da062322dc0dbb334d0d536c066aa0718b5
kei-kei-0420 <90430117+kei-kei-0420@users.noreply.github.com>
2021-09-13 11:07:35 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '左から1番目、上から4番目',
         },
         stage2: {
           q1: '豚肉',
